6deb9c759e9641e28fd1a40dd34f79b45b946036
TheSpectre101 <hjpdepacto31@src.pshs.edu.ph>
2026-08-13 14:31:08 +0800

    Update Act1_Depacto.py

--- Act1_Depacto.py
+++ Act1_Depacto.py
@@ -1,11 +1,16 @@
 import math
 def main ():
+  # Gets the x1, x2, y1, y2 from the user
+  
   x1 = float(input("Enter x1: "))
   y1 = float(input("Enter y1: "))
   x2 = float(input("Enter x2: "))
   y2 = float(input("Enter y2: "))
   distance (x1, y1, x2, y2)
 def distance (x1, y1, x2, y2):
+  
+  # Calculates the distance of x1, x2, y1, y2
+
   distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
 
   print("The distance between the two points is:", round(distance, 2))

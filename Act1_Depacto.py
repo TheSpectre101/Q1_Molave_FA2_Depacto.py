dfe8f86ea76de8f09513158893d4375751e0ed15
TheSpectre101 <hjpdepacto31@src.pshs.edu.ph>
2026-08-13 13:58:30 +0800

    Add files via upload

---
+++ Act1_Depacto.py
@@ -0,0 +1,16 @@
+import math
+
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
+
+distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
+
+print("The distance between the two points is:", round(distance, 2))
+
+# Reflection:
+# Using a math library makes the program easier because I can use
+# sqrt() and pow() instead of writing those calculations from scratch.
+# Without the library, calculating the square root and powers would
+# make the program longer and more difficult to understand.
